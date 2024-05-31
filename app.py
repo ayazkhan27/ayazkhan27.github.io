2e0cb7498d3a9542beb810111df469dcc78cb465
ayazkhan27 <79114819+ayazkhan27@users.noreply.github.com>
2024-05-31 04:41:10 -0400

    dad

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify, render_template
-
 import importlib.util
 import sys
+import time
 from decimal import Decimal, getcontext
 
 # Import the khan_encryption2.0 module
 module_name = "khan_encryption2.0"
-file_path = "khan_encryption_2.py"  # Update this to match the actual file path
+file_path = "C:/Users/admin/Documents/GitHub/ayazkhan27.github.io/khan_encryption_2.py"
 spec = importlib.util.spec_from_file_location(module_name, file_path)
 ke = importlib.util.module_from_spec(spec)
 sys.modules[module_name] = ke
@@ -34,16 +34,22 @@ def encrypt():
         prime = 1051
         cyclic_sequence = generate_cyclic_sequence(prime, prime - 1)
 
+        start_time = time.time()
         ciphertext, char_to_movement, movement_to_char, z_value, superposition_sequence, iv, salt, z_layers = ke.khan_encrypt(
             plaintext, prime, cyclic_sequence, start_position, superposition_sequence_length
         )
+        encryption_time = time.time() - start_time
+
+        start_time = time.time()
         decrypted_text = ke.khan_decrypt(
             ciphertext, char_to_movement, movement_to_char, z_value, superposition_sequence, iv, salt, z_layers, prime, start_position, cyclic_sequence
         )
+        decryption_time = time.time() - start_time
 
         response = {
-            'ciphertext': ''.join(map(str, ciphertext)),
-            'decryptedMessage': decrypted_text
+            'encryptionTime': f"{encryption_time:.6f}",
+            'decryptionTime': f"{decryption_time:.6f}",
+            'decryptedText': decrypted_text
         }
 
         return jsonify(response)
